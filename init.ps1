86582b7a77ab27f1e33b743af43f3004ffb71b6a
taidalog <72747279+taidalog@users.noreply.github.com>
2024-10-13 11:05:14 +0900

    Created an initialization file. #17

---
+++ init.ps1
@@ -0,0 +1,27 @@
+dotnet new console --language "F#" --name "App" --framework "net8.0" --output src
+dotnet new globaljson --sdk-version "8.0.100" --roll-forward latestFeature
+dotnet new nugetconfig
+dotnet new tool-manifest
+dotnet tool install fable
+dotnet tool install fantomas
+dotnet add .\src\App.fsproj package Fable.Core
+dotnet add .\src\App.fsproj package Fable.Browser.Dom
+dotnet new xunit --language "F#" --framework "net8.0" --output tests
+dotnet add .\tests\tests.fsproj reference .\src\App.fsproj
+# Save workspace.
+# Setup formatting.
+# Edit `.gitignore`
+#     Add Fable gererated files.
+npm init -y
+npm install -D vite
+New-Item -Name vite.config.ts -ItemType File
+# Edit vite.config.ts
+#     Add `base: '/<REPO>/'`
+#     Add `build` option
+#     Add `server.port` option
+New-Item -Name index.html -ItemType File
+New-Item -Name style.css -ItemType File
+# `dotnet fable` after cd .\src\
+# Edit index.html
+# Edit package.json
+# npm run build #at root, for `dotnet fable .\src\App.fsproj --run npx vite build`
