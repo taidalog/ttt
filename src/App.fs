3e26d7f5d0c2914ad99f4b42bc71d27f42f12ca4
taidalog <72747279+taidalog@users.noreply.github.com>
2024-10-13 12:23:00 +0900

    Changed a source file for Fable 4.22.0. #17

--- src/App.fs
+++ src/App.fs
@@ -196,7 +196,7 @@ module App =
             [ "helpButton"; "helpClose" ]
             |> List.iter (fun x ->
                 (document.getElementById x :?> HTMLButtonElement).onclick <-
-                    fun _ -> (document.getElementById "helpWindow").classList.toggle "active")
+                    fun _ -> (document.getElementById "helpWindow").classList.toggle "active" |> ignore)
 
             // information policy window
             (document.getElementById "informationPolicyLink").onclick <-
