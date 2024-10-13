63434737d399d8902608611c50c77faae06b1e28
taidalog <72747279+taidalog@users.noreply.github.com>
2024-10-13 11:49:09 +0900

    Restored test files from backup. #17

---
+++ tests/Types.Tests.fs
@@ -0,0 +1,34 @@
+// ttt Version 0.1.0
+// https://github.com/taidalog/ttt
+// Copyright (c) 2024 taidalog
+// This software is licensed under the MIT License.
+// https://github.com/taidalog/ttt/blob/main/LICENSE
+
+module Types.tests
+
+open System
+open Xunit
+open Ttt.Types
+
+let ``Date'.fst 1`` () =
+    let expected = DateTime(2024, 9, 22)
+    let actual = Date'.Single(DateTime(2024, 9, 22)).fst
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``Date'.fst 2`` () =
+    let expected = DateTime(2024, 9, 22)
+    let actual = Date'.Dudation(DateTime(2024, 9, 22), DateTime(2025, 10, 3)).fst
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``Date' 1`` () =
+    let expected = "9/22"
+    let actual = Date'.Single(DateTime(2024, 9, 22)).ToString()
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``Date' 2`` () =
+    let expected = "9/22~10/3"
+    let actual = Date'.Dudation(DateTime(2024, 9, 22), DateTime(2024, 10, 3)).ToString()
+    Assert.Equal(expected, actual)
